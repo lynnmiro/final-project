c7bcd74876e9a570b3c7f5d0464c4d7025c607ed
Desmend Jetton <djetton@iit.edu>
2018-05-01 19:37:00 -0500

    changed name of jade to pug for view engine and put app.set first

--- Fitness-App/app.js
+++ Fitness-App/app.js
@@ -15,12 +15,13 @@ var app = express();
 var path = require('path');
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+ app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views');
 // app.use(cookieParser());
