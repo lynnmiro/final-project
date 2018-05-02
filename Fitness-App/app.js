7ce1424b622936f58cc6e0c20113db801d3348a0
Desmend Jetton <djetton@iit.edu>
2018-05-02 10:13:34 -0500

    I commented out the app.js to try debugging. Changed display in pug

--- Fitness-App/app.js
+++ Fitness-App/app.js
@@ -1,6 +1,6 @@
 // var createError = require('http-errors');
 var express = require('express');
-// var path = require('path');
+var path = require('path');
 // var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
@@ -12,18 +12,15 @@ var pug = require('pug');
 var workouts = require('./routes/workouts');
 
 var app = express();
-var path = require('path');
 
 // view engine setup
- app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.set('view engine', 'pug')
-app.set('views', __dirname + '/views');
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
