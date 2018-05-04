118249c0ce9bea143697c53dfe32737c660d1aee
Desmend Jetton <djetton@iit.edu>
2018-05-04 10:57:56 -0500

    debugging register routes

---
+++ fitness/models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,    
+    trim: true
+  },
+});
+module.exports = mongoose.model('user', UserSchema);
+
--- fitness/routes/register.js
+++ fitness/routes/register.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let mongoose = require('mongoose');
-var User = require('../models/users')
+var User = require('../models/user')
 
 //Get registration page from index button//
 router.get('/register', function(req, res, next) {
@@ -12,23 +12,22 @@ router.get('/register', function(req, res, next) {
 
 //Post user data to database POST /register //
 router.post('/register', function(req, res, next) {
-   res.render('Welcome to Fit 7');
+   res.render('/home');
 })
-//    var username = req.body.username;
-//    var email = req.body.email;
-//    var password = req.body.password;
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
 
-//    var newuser =  new User();
-//    newuser.username = username;
-//    newuser.email = email;
-//    newuser.password = password;
-//    newuser.save(function(err, savedUser) {
-//        if(err) {
-//            console.log(err);
-//            return res.status(500).send();
-//        }
-        return res.status(200).send();
-     
-
+    var newuser =  new User();
+    newuser.username = username;
+    newuser.email = email;
+    newuser.password = password;
+    newuser.save(function(err, savedUser) {
+    //    if(err) {
+            console.log(err);
+            return res.status(500).send();
+    //    } else {
+    //    return res.status(200).send();
+    });
 
     module.exports = router;
